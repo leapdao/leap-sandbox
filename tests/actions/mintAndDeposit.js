5837eefb041bdad84919fa1b2fbdd39325c3a176
Evgeni <evgeni@leapdao.org>
2019-02-26 16:02:25 +0100

    changed transfer to actual mint

--- tests/actions/mintAndDeposit.js
+++ tests/actions/mintAndDeposit.js
@@ -6,9 +6,8 @@ module.exports = async function(alice, amount, minter, token, exitHandler, noLog
   
   log(`------Will mint and deposit ${amount} tokens for ${alice}------`);
   log(`Minting token to account ${alice}...`);
-  //await token.methods.mint(alice, amount).send({from: minter});
   const balanceOrig = (await token.methods.balanceOf(alice).call()) * 1;
-  await token.methods.transfer(alice, amount).send({from: minter});
+  await token.methods.mint(alice, amount).send({from: minter});
   const balanceMint = (await token.methods.balanceOf(alice).call()) * 1;
   log(`Aprroving ${amount} tokens for deposit...`);
   await token.methods.approve(exitHandler.options.address, amount).send({from: alice});
