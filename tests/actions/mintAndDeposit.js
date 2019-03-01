af3500fde498c34974347a25b07faf3ef5a30cfd
Kosta Korenkov <kosta@whoot.me>
2019-03-02 02:46:29 +0300

    add waitForBalanceChange helper

--- tests/actions/mintAndDeposit.js
+++ tests/actions/mintAndDeposit.js
@@ -1,15 +1,16 @@
-const { getLog, advanceBlocks } = require('../../src/helpers');
-const should = require('chai').should();
+const { getLog } = require('../../src/helpers');
+const waitForBalanceChange = require('./waitForBalanceChange');
+require('chai').should();
 
 module.exports = async function(alice, amount, minter, token, exitHandler, node, web3, noLog = false) {
   const log = getLog(noLog);
-  const oldPlasmaBalance = Number(await node.web3.eth.getBalance(alice));
+  const oldPlasmaBalance = await node.getBalance(alice);
 
   log(`------Will mint and deposit ${amount} tokens for ${alice}------`);
   log(`Minting token to account ${alice}...`);
-  const balanceOrig = (await token.methods.balanceOf(alice).call()) * 1;
+  const balanceOrig = Number(await token.methods.balanceOf(alice).call());
   await token.methods.mint(alice, amount).send({from: minter});
-  const balanceMint = (await token.methods.balanceOf(alice).call()) * 1;
+  const balanceMint = Number(await token.methods.balanceOf(alice).call());
   log(`Aprroving ${amount} tokens for deposit...`);
   await token.methods.approve(exitHandler.options.address, amount).send({from: alice});
   log(`Depositing ${amount} tokens...`);
@@ -17,16 +18,9 @@ module.exports = async function(alice, amount, minter, token, exitHandler, node,
     from: alice,
     gas: 2000000
   });
-  const balanceFinal = (await token.methods.balanceOf(alice).call()) * 1;
+  const balanceFinal = Number(await token.methods.balanceOf(alice).call());
   
-  let currentPlasmaBalance, i = 0;
-  do {
-    i++;
-    await advanceBlocks(1, web3);
-    currentPlasmaBalance = Number(await node.web3.eth.getBalance(alice));
-    process.stdout.write(`\rWaiting for deposit to mature. Root chain blocks passed: ${i}`);
-  } while(currentPlasmaBalance === oldPlasmaBalance)
-  console.log();
+  const currentPlasmaBalance = await waitForBalanceChange(alice, oldPlasmaBalance, node, web3);
   currentPlasmaBalance.should.be.equal(oldPlasmaBalance + amount);
   
   balanceMint.should.be.equal(balanceOrig + amount);
