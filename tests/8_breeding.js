4a8c519795908cfa111b480ff903d57092370cd3
Kosta Korenkov <kosta@whoot.me>
2019-06-14 20:55:19 +0300

    fix: change checkSpendingCondition according to leapdao/leap-node#242

--- tests/8_breeding.js
+++ tests/8_breeding.js
@@ -1,11 +1,13 @@
 const assert = require('assert');
 const ethUtil = require('ethereumjs-util');
-const { Tx, Input, Output, Outpoint } = require('leap-core');
+const { Tx, Input, Output, Outpoint, Consensus } = require('leap-core');
 
 const exitUnspent = require('./actions/exitUnspent');
 const minePeriod = require('./actions/minePeriod');
 
 const TOKEN = require('../build/contracts/build/contracts/ERC1949.json');
+
+const { getAddress } = Consensus.spendingCondition;
 
 const BreedingCondition =
   '6080604052348015600f57600080fd5b5060043610602b5760e060020a6000350463689dcafc81146030575b600080fd5b606960048036036080811015604457600080fd5b50803590600160a060020a03602082013581169160408101359160609091013516606b565b005b6040805160e060020a63451da9f902815260048101869052600160a060020a038581166024830152604482018590529151839283169163451da9f991606480830192600092919082900301818387803b15801560c657600080fd5b505af115801560d9573d6000803e3d6000fd5b50505050505050505056fea165627a7a72305820ff50695e9e2f7357f76cac1b8adb0d5d43b7930e23a23bdae6c9f81c5fcddfcc0029';
@@ -91,8 +93,7 @@ module.exports = async function(contracts, [node], accounts, web3) {
   assert.equal(unspents[0].output.data, tokenData, 'tokenData should match');
 
   const script = Buffer.from(BreedingCondition, 'hex');
-  const scriptHash = ethUtil.ripemd160(script);
-  const spAddr = `0x${scriptHash.toString('hex')}`;
+  const spAddr = getAddress(script);
   // XXX: add support for approval in ERC1949
   let transferTx = Tx.transfer(
     [
