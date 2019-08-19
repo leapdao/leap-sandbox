4a737e2b386f942f9d23faa7b78387cc1ea720d9
Kosta Korenkov <kosta@whoot.me>
2019-08-19 20:16:58 +0200

    feat: allow to run single test

--- src/commands/test.js
+++ src/commands/test.js
@@ -7,6 +7,9 @@ require('chai').should();
 
 const getTests = async () => {
   const testPath = path.join(__dirname, '../../tests');
+  if (process.argv[2]) {
+    return [process.argv[2]];
+  };
   const tests = fs
     .readdirSync(testPath)
     .filter(fileName =>
