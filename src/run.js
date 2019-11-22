89eb229b06257fae00b04e58bb81c2a0e7a291d3
Kosta Korenkov <kosta@whoot.me>
2019-11-22 14:03:33 +0300

    fix: use network id 5777 so it works nicely with Bridge UI

--- src/run.js
+++ src/run.js
@@ -15,7 +15,7 @@ async function setupGanache(port, mnemonic) {
   return new Promise(
     (resolve, reject) => {
       console.log('Starting ganache..');
-      const srv = ganache.server({ locked: false, mnemonic, blockTime: 1 });
+      const srv = ganache.server({ locked: false, mnemonic, blockTime: 1, network_id: 5777 });
       srv.listen(port, (err) => {
         if (err) {
           return reject();
