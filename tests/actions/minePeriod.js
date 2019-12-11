3d34f24219fdc955a48c80522ad074322c4947d6
Kosta Korenkov <kosta@whoot.me>
2019-12-11 15:42:38 +0300

    chore: speed up tests by using minePeriod script.
    >1.5x faster execution of the whole test suite.

--- tests/actions/minePeriod.js
+++ tests/actions/minePeriod.js
@@ -1,41 +1,7 @@
-const { Period } = require('leap-core');
-const { makeTransfer, advanceBlocks } = require('../../src/helpers');
+const minePeriod = require('leap-guardian/scripts/minePeriod')
 
 module.exports = async (env) => {
-  const { contracts, nodes, accounts, wallet, plasmaWallet } = env;
-  const node = nodes[0];
-  const { addr, privKey } = accounts[0];
-  const currentBlock = Number((await node.getBlock('latest')).number);
-  const [, lastBlockInPeriod] = Period.periodBlockRange(currentBlock);
+  const { networkConfig, wallet, plasmaWallet } = env;
 
-  const submissions = [];
-  contracts.operator.on("Submission", (...args) => {
-    submissions.push(args);
-  });
-
-  for (let i = 0; i <= lastBlockInPeriod - currentBlock + 20; i++) {
-    const transfer = await makeTransfer(
-      node,
-      addr,
-      addr,
-      1000,
-      0,
-      privKey
-    );
-    await node.sendTx(transfer);
-    process.stdout.write(`\rPushing till the next period: ${currentBlock + i}/${lastBlockInPeriod + 1}`);
-    if (submissions.length > 0) {
-      await advanceBlocks(6, wallet);
-    }
-    const periodData = await plasmaWallet.provider.getPeriodByBlockHeight(currentBlock);
-    if (submissions.length > 0 && periodData) {
-      if (periodData) {
-        process.stdout.write(' ✅\n');
-        return;
-      }
-      process.stdout.write(' 🔴\n');
-      break;
-    }
-  }
-  throw new Error('Period wasn\'t submitted on time');
+  await minePeriod({ plasmaWallet, rootWallet: wallet, nodeConfig: networkConfig });
 }
