956f28a9031ee70c92c3ccc8cc2cc12c24649462
Jan <jan@parseclabs.org>
2020-03-05 17:16:39 +0100

    Some help for pr83.

--- tests/2_challengeExit.js
+++ tests/2_challengeExit.js
@@ -1,15 +1,9 @@
 const debug = require('debug');
 const mintAndDeposit = require('./actions/mintAndDeposit');
 const { transfer } = require('./actions/transfer');
-const exitUnspent = require('./actions/exitUnspent');
 const minePeriod = require('./actions/minePeriod');
 const { helpers, Tx, Util } = require('leap-core');
 const { bufferToHex } = require('ethereumjs-util');
-const exitHandler = require('../build/contracts/build/contracts/ExitHandler');
-
-require('chai').should();
-
-//let challengeExit = exitHandler.challengeExit();
 
 const log = debug('challengeExit');
 
@@ -22,89 +16,39 @@ module.exports = async function(env, addr, color) {
     const bob = accounts[6].addr;
     const amount = 10000000;
     
-    let txHash;
-    let txData;
     console.log("╔══════════════════════════════════════════╗");
     console.log("║   Test: Challenge exit after Transfer    ║");
     console.log("║Steps:                                    ║");
     console.log("║1. Deposit to Alice                       ║");
     console.log("║2. Trasfer from Alice to Bob              ║");
-    console.log("║3. Exit Alice                    ║");
-    console.log("║4. Challenge Alice exit                             ║");
-    //console.log(typeof(contracts.exitHandler.challengeExit()));
+    console.log("║3. Exit Alice                             ║");
+    console.log("║4. Challenge Alice exit                   ║");
     console.log("╚══════════════════════════════════════════╝");
-   
+
     await mintAndDeposit(accounts[2], amount, contracts.token, 0, contracts.exitHandler, wallet, plasmaWallet);
-   
-    let plasmaBalanceBefore = await node.getBalance(alice);
-     
-     //console.log("plasmaBalanceBefore", plasmaBalanceBefore);
-   
-    console.log('Making a few transfers..');
-    for (let i = 0; i < 2; i++) {
-       await transfer(alice, alicePriv, bob, '1000', node);
       
-       }
-   let plasmaBalanceAfTf = await node.getBalance(alice);
-   
-   
+    // Alice makes a transfer to Bob
+    const t1 = await transfer(alice, alicePriv, bob, '1000', node);   
     await minePeriod(env);
-    
-    
-    
-    
-     const unspents = await node.getUnspent(addr, color);
-    
-     const latestBlockNumber = (await node.getBlock('latest')).number;
-    
-     const latestSubmittedBlock = latestBlockNumber - latestBlockNumber % 32;
-    
-     const getIndex = async (unspents, lastBlock) => { 
-         for(let i=0; i<unspents.length; i++) { 
-                  txHash = unspents[i].outpoint.hash;
-                  txData = await node.getTransaction(bufferToHex(txHash));
-                  console.log("Unspent", i, "blocknumber:", txData.blockNumber);
-                  console.log("Is submitted?", txData.blockNumber < lastBlock); 
-                  if (txData.blockNumber < lastBlock) return i;
-          }
-          return -1; 
-     };
-     
-    
-    const unspentIndex = await getIndex(unspents, latestSubmittedBlock);
-    
-    if (unspentIndex === -1) { 
-          throw new Error("Can't exit, no unspents are in submitted periods found");
-     };
-    
-    const unspent = unspents[unspentIndex]; 
-    
-    const proof = await helpers.getProof( 
-                            plasmaWallet.provider, 
-                            txData, 
-                            {excludePrevHashFromProof: true } ); 
-   // console.log(proof);
-
-   //console.log(unspents); 
-    log("------Exit Alice------");
-    await exitUnspent(env, alice);
-     
-    let plasmaBalanceFinal = await node.getBalance(alice);
- 
-    //console.log("plasmaBalanceAfExit", plasmaBalanceFinal)
 
 
-    log("------Exit Bob------");
-    await exitUnspent(env, bob);
+    const transfer1 = await node.getTransaction(bufferToHex(t1.hash()));
+    const proofOfTransfer1 = await helpers.getProof( 
+        plasmaWallet.provider, 
+        transfer1, 
+        {excludePrevHashFromProof: true }
+    );
 
-   console.log("Challenging Alice's exit");
-   contracts.exitHandler.challengeExit([], proof, 0, 0, bob)
+
+    // Now:
+    // 1. Bob spends the utxo he got from Alice in transfer1.
+    // 2. Bob starts an exit with the utxo he just sent to alice, using contract.exitHandler.startExit
+    // 3. But we know the unspent-transaction-output (utxo) he is trying to exit is NOT unspent (he sent it to Alice)
+    // 4. We use the proof that he spent the utxo in contract.exitHandler.challengeExit
+    // 5. In the end, we make sure the Exit struct in the exitHandler contract was deleted (this means the challenge was successful)
 
 
- 
- 
- //console.log("plasmaBalanceAfExit", 
-
+    
 }
 
 
