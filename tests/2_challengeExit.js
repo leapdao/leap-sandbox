9f5f5473c18315a8b1c565859c4e6816303c8908
Adetayo Timilehin <timilehin1999@gmail.com>
2020-03-21 12:57:50 +0100

    Update 2_challengeExit.js

--- tests/2_challengeExit.js
+++ tests/2_challengeExit.js
@@ -104,7 +104,7 @@ module.exports = async function(env, addr, color) {
     // 5. In the end, we make sure the Exit struct in the exitHandler contract was deleted (this means the challenge was successful)
 
       const t2 = await transfer(bob, bobPriv, charlie, '200', node);   
-      await minePeriod(env);
+      //await minePeriod(env);
     
       const transfer2 = await node.getTransaction(bufferToHex(t2.hash()));
        const proofOfTransfer2 = await helpers.getProof( 
