ed92b8f0a4c6cf47c27d173c7bda1ffd77b2f10f
Adetayo Timilehin <timilehin1999@gmail.com>
2020-03-21 12:33:02 +0100

    Update 2_challengeExit.js

--- tests/2_challengeExit.js
+++ tests/2_challengeExit.js
@@ -79,12 +79,17 @@ module.exports = async function(env, addr, color) {
         {excludePrevHashFromProof: true }
     );
     
+    await minePeriod(env);
+    
     const youngestInput = await helpers.getYoungestInputTx(node, Tx.fromRaw(transfer1.raw));
+    /*
     const youngestInputProof = await helpers.getProof( 
         plasmaWallet.provider, 
         youngestInput,  
         {excludePrevHashFromProof: true }
     );
+    
+    */
    // await minePeriod(env);
     
    // console.log('youngest input', youngestInput);
